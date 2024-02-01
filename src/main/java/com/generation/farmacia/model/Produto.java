e1070741fb73f41d6cdfe6515e75f2a60d702d84
vitoriahele <vitoriahele2013@gmail.com>
2024-02-01 12:41:16 -0300

    produto controller model repository add

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,19 +1,15 @@
 package com.generation.farmacia.model;
 
 import java.math.BigDecimal;
-import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.generation.farmacia.model.Produto;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -21,8 +17,8 @@ import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="tb_categorias")
-public class Categoria {
+@Table(name="tb_produtos")
+public class Produto {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -37,24 +33,13 @@ public class Categoria {
 	private String descricao;
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING)
-	@NotNull(message = "Preço é obrigatório!")
-	@Positive(message = "O preço deve ser maior do que zero!")
-	private BigDecimal preco;
-
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
-	@JsonIgnoreProperties("categoria")
-	private List <Produto> produto ;
-
-	public Categoria(Long id, String nome, String descricao) {
-		this.id = id;
-		this.nome = nome;
-		this.descricao = descricao;
-	}
+    @NotNull(message = "Preço é obrigatório!")
+    @Positive(message = "O preço deve ser maior do que zero!")
+    private BigDecimal preco;
 	
-	public Categoria() {
-	}
-
-
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+    private Categoria categoria;
 
 	public Long getId() {
 		return id;
@@ -79,7 +64,7 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
+
 	public BigDecimal getPreco() {
 		return preco;
 	}
@@ -87,14 +72,6 @@ public class Categoria {
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
 	}
-
-	public List<Produto> getProduto() {
-		return produto;
-	}
-
-	public void setProduto(List<Produto> produto) {
-		this.produto = produto;
-	}
-
+	
 	
 }
